1075f10cffdff3d178e7c75a851482a267279f28
Moritz <moritz.frankerl@googlemail.com>
2017-01-16 15:43:10 +0100

    calc done

--- sheet4/calc2.py
+++ sheet4/calc2.py
@@ -1,20 +1,45 @@
 from tkinter import *
+import operator
 
-
+wert = 0
+op = 0
 calculator = Tk()
+operators={"+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.truediv}
 
 screen = Label(calculator)
 val =StringVar()
+# variables=[]
 screen.configure(textvariable=val)
 screen.grid(row=6, column=1, columnspan=3)
+val.set(0)
 
 def set_val(num):
-    val.set(num)
+    if val.get() in ['+', '-', '*', '/', '=']:
+        val.set(0)
+    zahl=float(val.get())
+    zahl=zahl*10+num
+    val.set(zahl)
+    # variables.append(val)
 
+def set_operation(sign):
+    if op != 0:
+        wert = op(wert, float(val.get()))
+        print(wert)
+
+    if sign == '=':
+        val.set(wert)
+
+    else:
+        global op
+        global wert
+        wert = float(val.get())
+        val.set(sign)
+        op = operators[sign]
+        print(op)
 j=1
 
-for sign in ['+', '-', '*', '/']:
-    button = Button(calculator, text=sign, command=lambda sign=sign: set_val(sign))
+for sign in ['+', '-', '*', '/', '=']:
+    button = Button(calculator, text=sign, command=lambda sign=sign: set_operation(sign))
     button.grid(row=1, column=j)
     j+=1
 
