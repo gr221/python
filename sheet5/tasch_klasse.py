2eb759983b978be31487a210a9619ea825db2619
Moritz <moritz.frankerl@googlemail.com>
2017-01-20 15:23:10 +0100

    nice

--- sheet5/tasch_klasse.py
+++ sheet5/tasch_klasse.py
@@ -3,21 +3,25 @@ from tkinter import *
 class Taschenrechner(object):
     def __init__(self,app):
         screen = Label(app)
+        self.user_input = 0
         val =StringVar()
         screen.configure(textvariable=val)
         screen.grid(row=6, column=1, columnspan=3)
-        val.set(0)
         for index in [1,2,3,4,5,6,7,8,9,0]:
             b = Button(app)
-            b.configure(text=str(index))
-            b.configure(command=self.set_val(index))
+            b.configure(text=index)
+            b.configure(command=lambda index=index : self.set_val(index, val))
+            # b.configure(command= self.set_val(index, val,user_input))
             b.grid(column=(index-1)%3+1,row=(index-1)//3+1)
         b.grid(column=2,row=4)
         self.wert = 0
 
-    def set_val(self,index):
-        def protect():
-            val.set(index)
+    def set_val(self,index,val,):
+        print(self.user_input)
+        print(index)
+        self.user_input = 10*self.user_input +index
+        print(self.user_input)
+        val.set(self.user_input)
 
 
 app = Tk()
