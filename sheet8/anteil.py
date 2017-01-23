955a51a9ae8af7647b4b7a08da719b0be54f9226
Moritz <moritz.frankerl@googlemail.com>
2017-01-23 19:09:06 +0100

    yiah

--- sheet8/anteil.py
+++ sheet8/anteil.py
@@ -2,4 +2,16 @@ import pylab as pl
 
 phys = {
         'm': [87, 166, 41, 127, 27, 89, 13, 34, 18],
-        'f': [38,  43, 12,  15,  4, 14,  2,  3,  0]}
+        'f': [38,  43, 12,  15,  4, 14,  2,  3,  0]
+        }
+labels = 'Male', 'Female'
+
+for i in range(1,10):
+    x=330+i
+    sizes = [phys['m'][i-1], phys['f'][i-1]]
+    pl.subplot(x)
+    title = "Semster " +str(i)
+    pl.title(title)
+    pl.pie(sizes, labels = labels,autopct='%.2f%%', radius = 1)
+
+pl.show()
