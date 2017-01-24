773bbfc439291a4e1b83a16c8855bbcbbf2793bd
Moritz <moritz.frankerl@googlemail.com>
2017-01-24 14:36:31 +0100

    plotting is not always fun

---
+++ sheet8/mensa.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+f = open("mensaplan50.csv", "r")
+essen = f.readlines()
+f.close()
+#Dicts für die Wwerte
+zahl_essen={'Mo': 0, 'Di': 0, 'Mi': 0, 'Do': 0, 'Fr': 0}
+zahl_veggie={'Mo': 0, 'Di': 0, 'Mi': 0, 'Do': 0, 'Fr': 0}
+essen.pop(0)
+#Zähle wie viele essen es gibt und wie viele veggie essen
+for i in range(0,len(essen)):
+    zahl_essen[essen[i].split(';')[1]] +=1
+    if essen[i].split(';')[4] == 'V' or essen[i].split(';')[4]=='VG':
+        zahl_veggie[essen[i].split(';')[1]] +=1
+
+#Plotten
+index = ['Mo', 'Di', 'Mi', 'Do', 'Fr']
+left= np.linspace(0,4,5)
+bar_width=0.4
+rect1 = plt.bar(left-bar_width, zahl_essen.values(),width=bar_width, label='Essen',color='y')
+rect2 = plt.bar(left, zahl_veggie.values(),width=bar_width, label='Veggie', color='g')
+plt.xticks(range(len(zahl_essen)), index)
+plt.legend()
+plt.show()
