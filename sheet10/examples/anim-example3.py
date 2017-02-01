b0fb3627781ed99a06a2168f812856cfc50da6d8
Moritz Frankerl <moritz.frankerl@stud.uni-regensburg.de>
2017-02-01 11:42:10 +0100

    d

--- sheet10/examples/anim-example3.py
+++ sheet10/examples/anim-example3.py
@@ -4,16 +4,19 @@ import matplotlib.animation as animation
 
 
 def update_line(num, data, line):
-    print(data[..., :num].shape)
+    # print(data[..., :num].shape)
+    print(num)
+    print(data[..., :num])
     line.set_data(data[..., :num])
-    print(line)
+    # print(line)
     return line,
 
 fig1 = plt.figure()
 
 data = np.random.rand(2, 25)
+print(data)
 lines = plt.plot([], [], 'r-')
-print(type(lines[0]))
+# print(type(lines[0]))
 plt.xlim(0, 1)
 plt.ylim(0, 1)
 plt.xlabel('x')
--- sheet10/lissajous_anim.py
+++ sheet10/lissajous_anim.py
@@ -3,3 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 
+t = np.linspace(0,2*np.pi,1000)
+x = np.sin(7*t)
+y = np.sin(11*t+3)
+z = np.sin(5*t-2)
